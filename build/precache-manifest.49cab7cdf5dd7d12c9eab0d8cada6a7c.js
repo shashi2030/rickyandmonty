8e0f52135567b06c60b0aa99276222198bb7ca1f
Shashi Singh <shasingh36@publicisgroupe.net>
2020-05-29 16:14:36 +0530

    update gitignore file

---
+++ build/precache-manifest.49cab7cdf5dd7d12c9eab0d8cada6a7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c6e5dc480b0ff507ac8f591bf4f67a2",
+    "url": "/rickyandmonty/index.html"
+  },
+  {
+    "revision": "50b95f3c081b59881fc2",
+    "url": "/rickyandmonty/static/css/main.f80badc8.chunk.css"
+  },
+  {
+    "revision": "084f7ceea6c5f364bd7c",
+    "url": "/rickyandmonty/static/js/2.a88bcba5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/rickyandmonty/static/js/2.a88bcba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50b95f3c081b59881fc2",
+    "url": "/rickyandmonty/static/js/main.798c307a.chunk.js"
+  },
+  {
+    "revision": "4ed87467b66800f927cd",
+    "url": "/rickyandmonty/static/js/runtime-main.d8681d54.js"
+  },
+  {
+    "revision": "e75855a18e3532ccada0d25685656a95",
+    "url": "/rickyandmonty/static/media/logout.e75855a1.svg"
+  }
+]);
